92ee3e408ade3d5c5b982ccfc82cdeebee96a13b
DawidWraga <dpwraga@gmail.com>
2024-03-21 16:42:19 +0000

    test: finish testing setup

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -5,23 +5,23 @@ export default defineWorkspace([
 	// Vitest expects a list of config files
 	// or directories where there is a config file
 	'packages/*',
-	'tests/*/vitest.config.{e2e,unit}.ts',
-	// you can even run the same tests,
-	// but with different configs in the same "vitest" process
-	{
-		test: {
-			name: 'happy-dom',
-			root: './shared_tests',
-			environment: 'happy-dom',
-			setupFiles: ['./setup.happy-dom.ts'],
-		},
-	},
-	{
-		test: {
-			name: 'node',
-			root: './shared_tests',
-			environment: 'node',
-			setupFiles: ['./setup.node.ts'],
-		},
-	},
+	// 'tests/*/vitest.config.{e2e,unit}.ts',
+	// // you can even run the same tests,
+	// // but with different configs in the same "vitest" process
+	// {
+	// 	test: {
+	// 		name: 'happy-dom',
+	// 		root: './shared_tests',
+	// 		environment: 'happy-dom',
+	// 		setupFiles: ['./setup.happy-dom.ts'],
+	// 	},
+	// },
+	// {
+	// 	test: {
+	// 		name: 'node',
+	// 		root: './shared_tests',
+	// 		environment: 'node',
+	// 		setupFiles: ['./setup.node.ts'],
+	// 	},
+	// },
 ]);
