be0fd0aaa7a676deb9f13f94fe144603cc11a0eb
DawidWraga <dpwraga@gmail.com>
2024-03-24 17:12:01 +0000

    fix: comment out unused (temporarily)

--- packages/store/src/utils/extendSelectors.ts
+++ packages/store/src/utils/extendSelectors.ts
@@ -1,67 +1,67 @@
-import {
-  SelectorBuilder,
-  State,
-  StateActions,
-  StoreApi,
-  StoreApiGet,
-  StoreApiUse,
-  StoreApiUseTracked,
-} from '../types';
+// import {
+// 	SelectorBuilder,
+// 	State,
+// 	StateActions,
+// 	StoreApi,
+// 	StoreApiGet,
+// 	StoreApiUse,
+// 	StoreApiUseTracked,
+// } from '../types';
 
-export const extendSelectors = <
-  CB extends SelectorBuilder<TName, T, TActions, TSelectors>,
-  TName extends string,
-  T extends State = {},
-  TActions = {},
-  TSelectors = {},
->(
-  builder: CB,
-  api: StoreApi<TName, T, StateActions<T> & TActions, TSelectors>
-): StoreApi<
-  TName,
-  T,
-  StateActions<T> & TActions,
-  TSelectors & ReturnType<CB>
-> => {
-  const use = {
-    ...api.use,
-  } as StoreApiUse<T, TSelectors & ReturnType<CB>>;
+// export const extendSelectors = <
+// 	CB extends SelectorBuilder<TName, T, TActions, TSelectors>,
+// 	TName extends string,
+// 	T extends State = {},
+// 	TActions = {},
+// 	TSelectors = {},
+// >(
+// 	builder: CB,
+// 	api: StoreApi<TName, T, StateActions<T> & TActions, TSelectors>
+// ): StoreApi<
+// 	TName,
+// 	T,
+// 	StateActions<T> & TActions,
+// 	TSelectors & ReturnType<CB>
+// > => {
+// 	const use = {
+// 		...api.use,
+// 	} as StoreApiUse<T, TSelectors & ReturnType<CB>>;
 
-  const useTracked = {
-    ...api.useTracked,
-  } as StoreApiUseTracked<T, TSelectors & ReturnType<CB>>;
+// 	const useTracked = {
+// 		...api.useTracked,
+// 	} as StoreApiUseTracked<T, TSelectors & ReturnType<CB>>;
 
-  const get = {
-    ...api.get,
-  } as StoreApiGet<T, TSelectors & ReturnType<CB>>;
+// 	const get = {
+// 		...api.get,
+// 	} as StoreApiGet<T, TSelectors & ReturnType<CB>>;
 
-  Object.keys(builder(api.store.getState(), api.get, api)).forEach((key) => {
-    // @ts-ignore
-    use[key] = (...args: any[]) =>
-      api.useStore((state) => {
-        const selectors = builder(state, api.get, api);
-        const selector = selectors[key];
-        return selector(...args);
-      });
-    // @ts-ignore
-    useTracked[key] = (...args: any[]) => {
-      const trackedState = api.useTrackedStore();
-      const selectors = builder(trackedState, api.get, api);
-      const selector = selectors[key];
-      return selector(...args);
-    };
-    // @ts-ignore
-    get[key] = (...args: any[]) => {
-      const selectors = builder(api.store.getState(), api.get, api);
-      const selector = selectors[key];
-      return selector(...args);
-    };
-  });
+// 	Object.keys(builder(api.store.getState(), api.get, api)).forEach((key) => {
+// 		// @ts-ignore
+// 		use[key] = (...args: any[]) =>
+// 			api.useStore((state) => {
+// 				const selectors = builder(state, api.get, api);
+// 				const selector = selectors[key];
+// 				return selector(...args);
+// 			});
+// 		// @ts-ignore
+// 		useTracked[key] = (...args: any[]) => {
+// 			const trackedState = api.useTrackedStore();
+// 			const selectors = builder(trackedState, api.get, api);
+// 			const selector = selectors[key];
+// 			return selector(...args);
+// 		};
+// 		// @ts-ignore
+// 		get[key] = (...args: any[]) => {
+// 			const selectors = builder(api.store.getState(), api.get, api);
+// 			const selector = selectors[key];
+// 			return selector(...args);
+// 		};
+// 	});
 
-  return {
-    ...(api as any),
-    get,
-    use,
-    useTracked,
-  };
-};
+// 	return {
+// 		...(api as any),
+// 		get,
+// 		use,
+// 		useTracked,
+// 	};
+// };
