46589eb0ef17a78df41416c74cbebe9342f2cae8
DawidWraga <dpwraga@gmail.com>
2024-04-07 19:40:48 +0100

    chore: house keeping

--- examples/nextjs/src/app/page.tsx
+++ examples/nextjs/src/app/page.tsx
@@ -9,7 +9,7 @@ export default function AppPage(props: AppPageProps) {
 		<>
 			<div className="flex flex-col gap-2 p-5 ">
 				<Link href="/book-store">Book Store</Link>
-				<Link href="/counter">Counter</Link>
+				{/* <Link href="/counter">Counter</Link> */}
 				<Link href="/todo">Todo</Link>
 				<Link href="/todo-with-sound">Todo with sound</Link>
 			</div>
