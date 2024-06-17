8f0254377f95d702c021986565db59247363df1a
DawidWraga <dpwraga@gmail.com>
2024-06-17 14:52:24 +0100

    chore: set up react 19 linter

--- tooling/eslint/next.js
+++ tooling/eslint/next.js
@@ -9,6 +9,7 @@ module.exports = {
     "prettier",
     require.resolve("@vercel/style-guide/eslint/next"),
     "eslint-config-turbo",
+    "eslint-config-react-app"
   ],
   globals: {
     React: true,
