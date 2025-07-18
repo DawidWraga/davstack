ef02ab86d131b36b41f1bd7ab3c7349066dec0b3
DawidWraga <dpwraga@gmail.com>
2025-07-18 18:20:18 +0100

    zod 4 compatible

--- packages/fn/src/utils/type-utils.ts
+++ packages/fn/src/utils/type-utils.ts
@@ -9,33 +9,33 @@ export type Simplify<T> = T extends any[] | Date
 	? T
 	: { [K in keyof T]: T[K] } & {};
 
+//* MARK: OLD ------------
+// export type zInfer<T extends ZodTypeAny> = T['_output'];
+// export type zInferInput<T extends ZodTypeAny> = T['_input'];
+
+// export type ZodTypeAny = z3.ZodTypeAny;
+
+//* MARK: NEW ------------
+
 // NEW: Version-agnostic type inference for the schema's output type.
 // It uses conditional types to check if the provided schema is a v3 or v4 type.
-export type zInfer<T extends ZodTypeAny> = T['_output'];
-export type zInferInput<T extends ZodTypeAny> = T['_input'];
 
-export type ZodTypeAny = z3.ZodTypeAny;
+import * as z4 from 'zod/v4/core';
+export type zInfer<T> = T extends z3.ZodTypeAny
+	? z3.infer<T>
+	: T extends z4.$ZodType
+		? z4.output<T>
+		: never;
 
-// export function isZod4(schema: ZodTypeAny): schema is z4.$ZodType {
-// 	return '_zod' in schema;
-// }
-// // NEW: Version-agnostic type inference for the schema's output type.
-// // It uses conditional types to check if the provided schema is a v3 or v4 type.
-// export type zInfer<T> = T extends z3.ZodTypeAny
-// 	? z3.infer<T>
-// 	: T extends z4.$ZodType
-// 		? z4.output<T>
-// 		: never;
+// NEW: Version-agnostic type inference for the schema's input type.
+export type zInferInput<T> = T extends z3.ZodTypeAny
+	? z3.input<T>
+	: T extends z4.$ZodType
+		? z4.input<T>
+		: never;
 
-// // NEW: Version-agnostic type inference for the schema's input type.
-// export type zInferInput<T> = T extends z3.ZodTypeAny
-// 	? z3.input<T>
-// 	: T extends z4.$ZodType
-// 		? z4.input<T>
-// 		: never;
+export type ZodTypeAny = z3.ZodTypeAny | z4.$ZodType;
 
-// export type ZodTypeAny = z3.ZodTypeAny | z4.$ZodType;
-
-// export function isZod4(schema: ZodTypeAny): schema is z4.$ZodType {
-// 	return '_zod' in schema;
-// }
+export function isZod4(schema: ZodTypeAny): schema is z4.$ZodType {
+	return '_zod' in schema;
+}
